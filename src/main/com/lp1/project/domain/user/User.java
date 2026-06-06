6e5ffe6b4ee8b905e31e617206906369d74b133e
Augusto <augustocesaaaar@gmail.com>
2026-06-06 09:23:58 -0300

    fix user

--- src/main/com/lp1/project/domain/user/User.java
+++ src/main/com/lp1/project/domain/user/User.java
@@ -1,15 +1,14 @@
 package com.lp1.project.domain.user;
 
-import com.lp1.project.domain.address.Address;
-import com.lp1.project.domain.order.Order;
-import com.lp1.project.domain.product.Product;
-import com.lp1.project.domain.review.Review;
+import java.time.LocalDate;
 
-import java.util.List;
-import java.util.Set;
 
 public class User {
-    private Address principalAddress;
-    private List<Address> addresses;
-    private Set<Product> favorites;
+    private long id;
+    private String name;
+    private LocalDate birth;
+    private String cpf;
+    private String email;
+    private String password;
+    private String numberPhone;
 }
